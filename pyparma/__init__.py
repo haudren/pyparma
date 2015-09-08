b4b477f74b8cae096bceebb1b5cf1c30b3d4fbc8
Hervé Audren <h.audren@gmail.com>
2015-09-08 18:56:53 +0900

    Removed useless environment variable

--- pyparma/__init__.py
+++ pyparma/__init__.py
@@ -1,9 +1,3 @@
-import os
-#For finding the lib, we have to extend LD_LIBRARY_PATH
-path = os.path.dirname(os.path.realpath(__file__))
-print "Adding {} to the .so search path".format(path)
-os.environ['LD_LIBRARY_PATH'] = os.environ.get('LD_LIBRAY_PATH', '')+":"+path
-
 from ppl import *
 #Note: this import will override the standard Polyhedron
 #that is not instantiable anyway
