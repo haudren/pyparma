5cf6e2f03ce5b7362c1cfd925e8efdeaa462d603
Hervé Audren <h.audren@gmail.com>
2015-09-10 11:04:30 +0900

    Remove useless flags in setup

--- setup.py
+++ setup.py
@@ -14,21 +14,11 @@ setup(
     packages=["pyparma", "pyparma/utils"],
     cmdclass={'build_ext': build_ext},
     ext_modules=[
-        #Extension("pyparma.mpz_pylong",
-        #          sources=["pyparma/mpz_pylong/mpz_pylong.c"],
-        #          libraries=["gmp"],
-        #          language="c++",
-        #          extra_compile_args=["-fPIC"]
-        #          ),
-        Extension("pyparma.ppl",
+       Extension("pyparma.ppl",
             sources=["pyparma/ppl.pyx", "pyparma/ppl_shim.cc", "pyparma/mpz_pylong/mpz_pylong.c"],
                   libraries=["ppl", "gmpxx"],
                   language="c++",
-                  extra_compile_args=["-fPIC", "-I/usr/include/x86_64-linux-gnu/"],
-                  extra_link_args=["-L/usr/lib/x86_64-linux-gnu/",
-                                   "-L/home/herve/dev/pyparma/build/lib.linux-x86_64-2.7/pyparma",
-                                   "-L/usr/lib/python2.7/dist-packages",
-                                   "-fPIC"]
+                  extra_compile_args=["-fPIC"]
                   )
                 ]
 )
