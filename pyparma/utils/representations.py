e6da5dac90f016666b55bf1e08d6acb951f5a983
Hervé Audren <h.audren@gmail.com>
2015-09-11 14:03:17 +0900

    Now supports lines/rays and refactor a bit

--- pyparma/utils/representations.py
+++ pyparma/utils/representations.py
@@ -1,5 +1,5 @@
 from pyparma import C_Polyhedron, Constraint_System, Linear_Expression,\
-                    Generator_System, point
+                    Generator_System, point, line
 
 import numpy as np
 from fractions import Fraction
@@ -63,40 +63,44 @@ def reduce_lcm(iterable):
     coords = [d*n//de for n, de in zip(nums, denoms)]
     return coords, d
 
+def ex_from_line(l):
+    if isinstance(l[0], Fraction):
+        scaled, _ = reduce_lcm(l)
+        offset, coeffs = scaled[0], scaled[1:]
+    else:
+        offset, coeffs = l[0], l[1:]
+    return Linear_Expression(coeffs, offset)
+
+def gen_from_line(l):
+    t, coords = l[0], l[1:]
+    if isinstance(coords[0], int) or isinstance(coords[0], long):
+        d = 1
+    elif isinstance(coords[0], Fraction):
+        coords, d = reduce_lcm(coords)
+    ex = Linear_Expression(coords, 0)
+
+    if t == 1:
+        return point(ex, d)
+    elif t == 0:
+        return line(ex)
+
 #Storage format b | A where b + Ax >= 0
 def from_hrep(hrep):
     cs = Constraint_System()
     for l in hrep:
-        if isinstance(l[0], Fraction):
-            scaled, _ = reduce_lcm(l)
-            offset, coeffs = scaled[0], scaled[1:]
-        else:
-            offset, coeffs = l[0], l[1:]
-        #We should also convert from Fraction to integer
-        #ex = Linear_Expression(coeffs*lcm, offset*lcm)
-        ex = Linear_Expression(coeffs, offset)
+        ex = ex_from_line(l)
         cs.insert(ex >= 0)
     return C_Polyhedron(cs)
 
 def add_ineq(line, poly):
-    ex = Linear_Expression(line[0], line[1:])
+    ex = ex_from_line(line)
     poly.add_constraint(ex >= 0)
 
 #Storage format type | points : type = 1 for points
 def from_vrep(vrep):
     gs = Generator_System()
     for l in vrep:
-        t, coords = l[0], l[1:]
-        if t == 1:
-            if isinstance(coords[0], int) or isinstance(coords[0], long):
-                d = 1
-            elif isinstance(coords[0], Fraction):
-                coords, d = reduce_lcm(coords)
-            ex = Linear_Expression(coords, 0)
-            #TODO : Case when we have non-points i.e. rays/lines
-            gs.insert(point(ex, d))
-        else:
-            raise ValueError("Sorry, the wrapper does not implement rays/lines")
+        gs.insert(gen_from_line(l))
     return C_Polyhedron(gs)
 
 def add_point(point, poly):
