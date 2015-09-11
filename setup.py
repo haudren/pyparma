211980daa9687f3c1783be895cf2c24add65c53b
Hervé Audren <h.audren@gmail.com>
2015-09-11 14:02:23 +0900

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+#from distutils.extension import Extension
+from setuptools import Extension
 from Cython.Distutils import build_ext
+
+with open('README') as f:
+    long_desc = f.read()
+
+classif = ['Development Status :: 3 - Alpha',
+           'Intended Audience :: Developers',
+           'Intended Audience :: Science/Research',
+           'Topic :: Scientific/Engineering :: Mathematics',
+           'License :: OSI Approved :: GNU General Public License (GPL)',
+           'Programming Language :: Python :: 2.7']
 
 setup(
     name="pyparma",
     version="0.1.0",
-    description="Bindings to the parma polyhedra library, allowing to use double description from Python",
+    description="Bindings to the parma polyhedra library,\
+                 allowing to use double description from Python",
+    long_description=long_desc,
     url="https://github.com/haudren/pyparma",
     author="Hervé Audren",
+    author_email="h.audren@aist.go.jp",
     license="GPL",
+    classifiers=classif,
     packages=["pyparma", "pyparma/utils"],
     cmdclass={'build_ext': build_ext},
     ext_modules=[
-       Extension("pyparma.ppl",
-            sources=["pyparma/ppl.pyx", "pyparma/ppl_shim.cc", "pyparma/mpz_pylong/mpz_pylong.c"],
+        Extension("pyparma.ppl",
+            sources=["pyparma/ppl.pyx",
+                     "pyparma/ppl_shim.cc",
+                     "pyparma/mpz_pylong/mpz_pylong.c"],
                   libraries=["ppl", "gmpxx"],
                   language="c++",
                   extra_compile_args=["-fPIC"]
